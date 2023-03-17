7a03a585a1b2311c3e61bd615b7b6f950517c9dc
tero1115 <limjisang1115@gmail.com>
2023-03-17 11:28:41 +0900

    필터 및 인터셉터 만들기

---
+++ src/main/java/shop/mtcoding/hiberpc/config/filter/MyBlackListFilter.java
@@ -0,0 +1,33 @@
+package shop.mtcoding.hiberpc.config.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyBlackListFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // 주의 : 버퍼를 비우면, 컨트롤러에서 버퍼를 읽지 못한다
+        // x-www-form-urlencoded
+        String value = request.getParameter("value");
+
+        if (value == null) {
+            response.setContentType("text/plain; charset=utf-8");
+            response.getWriter().println("value 파라메터를 전송해주세요");
+            return;
+        }
+        if (value.equals("babo")) {
+            response.setContentType("text/plain; charset=utf-8");
+            response.getWriter().println("당신은 블랙리스트가 되었습니다");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+}
